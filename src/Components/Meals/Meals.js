47868f7ea2172721c996240bcaf2673761120b56
amirmtolouie@gmail.com <amirmtolouie@gmail.com>
2023-08-10 22:06:43 +0300

    Added Meals & MealItem & MealItemForm MealAvailable & MealsSummary & Card & Input Components

---
+++ src/Components/Meals/Meals.js
@@ -0,0 +1,13 @@
+import { Fragment } from "react";
+import MealsAvailable from "./MealsAvailable";
+import MealsSummary from "./MealsSummary";
+function Meals(){
+    return(
+        <Fragment>
+            <MealsSummary />
+            <MealsAvailable />
+        </Fragment>
+    ) 
+}
+
+export default Meals;
